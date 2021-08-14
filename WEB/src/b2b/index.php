8e6b2cbf37334c492887a52af9d204849b3cde2e
daxxramass <he201753@students.ephec.be>
2021-08-14 13:20:02 +0200

    [WEB-SERVICE] Hosting two websites on the PHP Apache Web Server
    Unfortunately, can't yet check if it works (need fully operational SOA).
    
    Added a custom configuration file for the php-apache docker image.
    
    Created a static html page to be served by the PHP Apache Web Server.

--- WEB/src/b2b/index.php
+++ WEB/src/b2b/index.php
@@ -1,27 +1,27 @@
 <?php
 
-echo "<h1>Bienvenue sur le site B2B de WoodyToys !</h1>";
+    echo "<h1>Bienvenue sur le site B2B de WoodyToys !</h1>";
 
-echo "<br>";
-echo "<br>";
-echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
 
-//These are the defined authentication environment in the db service
+    //These are the defined authentication environment in the db service
 
-// The MySQL service named in the docker-compose.yml.
-$host = 'db';
+    // The MySQL service named in the docker-compose.yml.
+    $host = 'db';
 
-// Database use name
-$user = 'admin';
+    // Database use name
+    $user = 'admin';
 
-//database user password
-$pass = 'admin';
+    //database user password
+    $pass = 'admin';
 
-// check the MySQL connection status
-$conn = new mysqli($host, $user, $pass);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL server successfully!";
-}
+    // check the MySQL connection status
+    $conn = new mysqli($host, $user, $pass);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        echo "Connected to MySQL server successfully!";
+    }
 ?>
